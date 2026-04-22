3f06013f107e2341300cb1771df099273decf734
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2026-04-22 07:52:58 -0700

    Updated to Unity 2020.3.49f1 + fix API breaks

--- Assets/Scripts/Management/UIManager.cs
+++ Assets/Scripts/Management/UIManager.cs
@@ -136,7 +136,6 @@ public class UIManager : MonoBehaviour
 
     public void TryStartPopup()
     {
-        AdManager.Instance.PlayRewardAd();
         CheckIfCanContinue();
         ClosePopup();
     }
